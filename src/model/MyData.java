dabf9c23ac33b3bd5f7c43779eefa5947e8cb061
prolific-dev <de391ago@htwg-konstanz.de>
2021-09-28 14:00:56 +0200

    setup linechart view

---
+++ src/model/MyData.java
@@ -0,0 +1,55 @@
+package model;
+
+import javafx.geometry.Point2D;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+
+public class MyData {
+    private List<Point2D> inner = new ArrayList<>();
+    private List<Point2D> hull = new ArrayList<>();
+
+    public MyData(File file) throws IOException {
+        GrahamScan grahamScan = getGrahamScan(getGeomCalcData(getConverterData(file)));
+        this.inner = setInnerList(grahamScan.getInner());
+        this.hull = setHullList(grahamScan.getOuter());
+
+    }
+
+    private List<Point2D> getConverterData(File file) throws IOException {
+        return new Converter(file).getInput();
+    }
+
+    private Map<Point2D, Double> getGeomCalcData(List<Point2D> point2DList) {
+        return new GeomCalc(point2DList).getAngleDataMap();
+    }
+
+    private GrahamScan getGrahamScan(Map<Point2D, Double> angleDataMap) {
+        return new GrahamScan(angleDataMap);
+    }
+
+    private List<Point2D> setInnerList(Map<Point2D, Double> innerData) {
+        List<Point2D> list = new ArrayList<>();
+        innerData.forEach((key, value) -> list.add(key));
+        return list;
+    }
+
+    private List<Point2D> setHullList(Stack<Map.Entry<Point2D, Double>> hullData) {
+        List<Point2D> list = new ArrayList<>();
+        hullData.stream().map(Map.Entry::getKey).forEach(list::add);
+        return list;
+    }
+
+    public List<Point2D> getInnerList() {
+        return this.inner;
+    }
+
+    public List<Point2D> getHullList() {
+        return this.hull;
+    }
+}
