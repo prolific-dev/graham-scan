63b600016d3704c6060780b25827c8516854fa2d
prolific-dev <de391ago@htwg-konstanz.de>
2021-10-06 13:24:52 +0200

    Restructuring ChartController.

--- src/main/java/com/prolificdev/controller/ChartController.java
+++ src/main/java/com/prolificdev/controller/ChartController.java
@@ -10,26 +10,30 @@ import java.io.File;
 import java.io.IOException;
 
 public class ChartController {
+    private final XYChart.Series<Number, Number> innerSeries = new XYChart.Series<>();
+    private final XYChart.Series<Number, Number> convexHullSeries = new XYChart.Series<>();
+    private File file;
+
     @FXML
     private LineChart<Number, Number> lineChart;
 
     @FXML
     public void initializeChart(ActionEvent event) throws IOException {
-        File file = new File("src/test/resources/com/prolificdev/data/grahamscan_test_data.txt");
-        GrahamScan grahamScan = new GrahamScan(file);
+        this.innerSeries.setName("Inner-Points");
+        this.convexHullSeries.setName("Convex-Hull");
 
-        XYChart.Series<Number, Number> innerSeries = new XYChart.Series<>();
-        XYChart.Series<Number, Number> convexHullSeries = new XYChart.Series<>();
+        if (file == null) {
+            this.file = new File("src/test/resources/com/prolificdev/data/grahamscan_test_data.txt");
+            GrahamScan grahamScan = new GrahamScan(file);
 
-        innerSeries.setName("Inner-Points");
-        convexHullSeries.setName("Convex-Hull");
+            grahamScan.getInnerPoints()
+                    .forEach(p -> this.innerSeries.getData().add(new XYChart.Data<>(p.getX(), p.getY())));
+            grahamScan.getConvexHull()
+                    .forEach(p -> this.convexHullSeries.getData().add(new XYChart.Data<>(p.getX(), p.getY())));
 
-        grahamScan.getInnerPoints().forEach(p -> innerSeries.getData().add(new XYChart.Data<>(p.getX(), p.getY())));
-        grahamScan.getConvexHull().forEach(p -> convexHullSeries.getData().add(new XYChart.Data<>(p.getX(), p.getY())));
-
-        lineChart.getData().add(innerSeries);
-        lineChart.getData().add(convexHullSeries);
-        lineChart.setLegendVisible(true);
-        lineChart.setCreateSymbols(true);
+            lineChart.getData().add(this.innerSeries);
+            lineChart.getData().add(this.convexHullSeries);
+            lineChart.setLegendVisible(true);
+        }
     }
 }
